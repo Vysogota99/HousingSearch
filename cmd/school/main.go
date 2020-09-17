ba10cea81a07808631b90ea57e2fec5e2e503ca5
Ivan <vanialapshin99@yandex.ru>
2020-09-17 21:55:53 +0300

    test commit

--- cmd/school/main.go
+++ cmd/school/main.go
@@ -9,7 +9,13 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "В голове было смешнее")
+		fmt.Fprintln(w, "Request url: "+r.URL.String())
+		fmt.Fprintln(w, "User agent: "+r.UserAgent())
+		fmt.Fprintln(w, "Method: "+r.Method)
+		fmt.Fprintln(w, "IP: "+r.RemoteAddr)
 	})
 
-	http.ListenAndServe(":3000", mux)
+	if err := http.ListenAndServe(":3000", mux); err != nil {
+		panic(err)
+	}
 }
