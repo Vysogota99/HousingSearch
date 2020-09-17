6a4cdd5e0d6cb64460cea65934c8b860189787e4
Ivan <vanialapshin99@yandex.ru>
2020-09-17 21:45:34 +0300

    fix bug

--- cmd/school/main.go
+++ cmd/school/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"fmt"
+	"net/http"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/test", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello world",
-		})
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "В голове было смешнее")
 	})
-	r.Run(":3000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	http.ListenAndServe(":3000", mux)
 }
