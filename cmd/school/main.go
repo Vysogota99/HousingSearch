08b6339189aaa5966b8e96fc9e1ab25b9a0ac40d
Ivan <vanialapshin99@yandex.ru>
2020-09-17 22:15:21 +0300

    add json to test route

--- cmd/school/main.go
+++ cmd/school/main.go
@@ -1,18 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
+
+// TestResp ...
+type TestResp struct {
+	Msg       string `json:"message"`
+	URL       string `json:"url"`
+	UserAgent string `json:"user-agent"`
+	Method    string `json:"http-method"`
+	IP        string `json:"IP"`
+}
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "В голове было смешнее")
-		fmt.Fprintln(w, "Request url: "+r.URL.String())
-		fmt.Fprintln(w, "User agent: "+r.UserAgent())
-		fmt.Fprintln(w, "Method: "+r.Method)
-		fmt.Fprintln(w, "IP: "+r.RemoteAddr)
+		resp := &TestResp{}
+		resp.Msg = "Hello world SaniyaShenpion"
+		resp.URL = r.URL.String()
+		resp.UserAgent = r.UserAgent()
+		resp.Method = r.Method
+		resp.IP = r.RemoteAddr
+
+		b, err := json.Marshal(resp)
+		if err != nil {
+			http.Error(w, "JSON marshal error", http.StatusInternalServerError)
+		}
+
+		w.Write(b)
 	})
 
 	if err := http.ListenAndServe(":3000", mux); err != nil {
