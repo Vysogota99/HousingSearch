3e1af3c361b0389f631f7bc36a3a4cf37bd35177
Ivan <vanialapshin99@yandex.ru>
2020-09-24 21:50:59 +0300

    fix bug with cors policy

--- cmd/school/main.go
+++ cmd/school/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
@@ -18,7 +19,7 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		resp := &TestResp{}
-		resp.Msg = "Hello world SaniyaShenpion"
+		resp.Msg = "Test message for front"
 		resp.URL = r.URL.String()
 		resp.UserAgent = r.UserAgent()
 		resp.Method = r.Method
@@ -29,10 +30,16 @@ func main() {
 			http.Error(w, "JSON marshal error", http.StatusInternalServerError)
 		}
 
+		w.Header().Add("Content-Type", "application/json")
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		
+		log.Println(resp)
 		w.Write(b)
 	})
 
+	log.Println("Starting server at http://127.0.0.1:3000")
 	if err := http.ListenAndServe(":3000", mux); err != nil {
+		log.Println(err)
 		panic(err)
 	}
 }
